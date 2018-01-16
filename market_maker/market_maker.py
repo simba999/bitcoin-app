3ab984deca4c81b214a4fd5a22d3765c909a931d
simba999 <oliverking8958@yahoo.com>
2018-01-15 17:33:07 -0800

    add plugins and headers in python

---
+++ market_maker/market_maker.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from time import sleep
+import sys
+from datetime import datetime
+from os.path import getmtime
+import random
+import requests
+import atexit
+import signal
+
+from market_maker import bitmex
+from market_maker.settings import settings
+from market_maker.utils import log, constants, errors, math
+# Used for reloading the bot - saves modified times of key files
+import os
+watched_files_mtimes = [(f, getmtime(f)) for f in settings.WATCHED_FILES]
+
+
+#
+# Helpers
+#
+logger = log.setup_custom_logger('root')
